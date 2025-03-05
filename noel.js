f2e47433be2815fd878c21d0cafa35bf951d2ddc
Noel Odero <anoel.odero@gmail.com>
2025-03-05 11:52:16 +0200

    Be my girl?

---
+++ noel.js
@@ -0,0 +1,7 @@
+function salutation(name){
+    return `Hello, ${name}. You are the most beautiful human I have ever laid eyes on! Do you have a boyfriend?`
+
+
+}
+
+console.log(salutation("Noel"))
